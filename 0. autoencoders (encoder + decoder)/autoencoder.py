5b9d659721002b28a0b8c85b1185b3b9856de0ab
dongping c <122350071+dchen376@users.noreply.github.com>
2024-04-13 10:50:22 -0700

    save & load autoencoder
    done with all autoencoder implementations.

--- 0. autoencoders (encoder + decoder)/autoencoder.py
+++ 0. autoencoders (encoder + decoder)/autoencoder.py
@@ -1,3 +1,6 @@
+import os
+import pickle
+
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Conv2D, ReLU,BatchNormalization, Flatten, Dense, Reshape, Conv2DTranspose, Activation
 from tensorflow.keras import backend as K
@@ -55,6 +58,45 @@ class Autoencoder:
                        shuffle = True
                        )
 
+
+    def save(self, save_folder ="."):
+        self._create_folder_if_it_doesnt_exist(save_folder)
+        self._save_parameters(save_folder)
+        self._save_weights(save_folder)
+
+
+    @classmethod
+    def load(cls, save_folder="."):
+        parameters_path = os.path.join(save_folder, "parameters.pkl")
+        with open(parameters_path, "rb") as f:
+            parameters = pickle.load(f)
+        autoencoder = Autoencoder(*parameters) # *-> for unpacking iterable objects like lists.
+        weights_path = os.path.join(save_folder, "weights.h5")
+        autoencoder.load_weights(weights_path)
+        return autoencoder
+
+    def load_weights(self, weights_path):
+        self.model.load_weights(weights_path)
+
+    def _create_folder_if_it_doesnt_exist(self, folder):
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+    def _save_parameters(self, save_folder):
+        parameters = [
+            self.input_shape,
+            self.conv_filters,
+            self.conv_kernels,
+            self.conv_strides,
+            self.latent_space_dim
+        ]
+        save_path = os.path.join(save_folder, "parameters.pkl")
+        with open(save_path, "wb") as f:
+            pickle.dump(parameters, f)
+
+    def _save_weights(self, save_folder):
+        save_path = os.path.join(save_folder, "weights.h5")
+        self.model.save_weights(save_path)
 
     def _build(self):
         self._build_encoder()
