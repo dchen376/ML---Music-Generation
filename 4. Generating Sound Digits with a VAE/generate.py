f97732bfc9b4f823592fcb6690b0c1d421901a3d
dongping c <122350071+dchen376@users.noreply.github.com>
2024-04-14 02:25:04 -0700

    final step - generation of Sound
    - generate audios from spectrograms (final step!)

---
+++ 4. Generating Sound Digits with a VAE/generate.py
@@ -0,0 +1,80 @@
+import os
+import pickle
+
+import numpy as np
+import soundfile as sf
+
+from soundgenerator import SoundGenerator
+from vae import VAE
+
+from train import SPECTROGRAMS_PATH
+
+HOP_LENGTH = 256
+SAVE_DIR_ORIGINAL = r"D:\personal projects\Music Generation\samples\SAVE_DIR_ORIGINAL"
+SAVE_DIR_GENERATED = r"D:\personal projects\Music Generation\samples\SAVE_DIR_GENERATED"
+MIN_MAX_VALUES_PATH = r"D:\personal projects\Music Generation\samples\MIN_MAX_VALUES_SAVE_DIR\min_max_values.pkl"
+
+"""
+This file is used to generate and save the spectrograms gotten from the FSDD
+"""
+
+def load_fsdd(spectrograms_path):
+    x_train = [] #an empty list to be filled with spectrograms.
+    file_paths = []
+    for root, _, file_names in os.walk(spectrograms_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            spectrogram = np.load(file_path) # now, had the spectrogram.
+            # (n_bins which from stft, n_frames, 1) #1 is the extra dimension.
+            x_train.append(spectrogram) # stored in x_train list.
+            file_paths.append(file_path)
+    x_train = np.array(x_train) # cast to numpy array.
+    x_train = x_train[..., np.newaxis] # np.newaxis is used to increase the dimensionality of an array by one unit. # -> (3000 # of data we have in dataset, 256 num bins, 64 frames, 1) # treat spectrograms as grey scale images.
+    return x_train, file_paths
+
+# function to SAMPLE a few spectrograms.
+def select_spectrograms(spectrograms,
+                        file_paths,
+                        min_max_values,
+                        num_spectrograms = 2):
+    sampled_indexes = np.random.choice(range(len(spectrograms)), num_spectrograms)
+    sampled_spectrograms = spectrograms[sampled_indexes]
+    file_paths = [file_paths[index] for index in sampled_indexes]
+    sampled_min_max_values = [min_max_values[file_path] for file_path in file_paths]
+
+    print(file_paths)
+    print(sampled_min_max_values)
+    return sampled_spectrograms, sampled_min_max_values
+
+def save_signals(signals, save_dir, sample_rate = 22050):
+    for i, signal in enumerate(signals):
+        save_path = os.path.join(save_dir, str(i) + ".wav")
+        sf.write(save_path, signal, sample_rate) #write into the sound file sf.
+
+
+
+if __name__ == "__main__":
+    # initialise sound generator
+    vae = VAE.load("model") # in model file
+    sound_generator = SoundGenerator(vae, HOP_LENGTH)
+
+    # load spectrograms + min_max_values
+    with open(MIN_MAX_VALUES_PATH, "rb") as f:
+        min_max_values = pickle.load(f) # pickle.load(f) is a Python function used to deserialize a binary file containing a Python object serialized with the pickle module
+    specs, file_paths = load_fsdd(SPECTROGRAMS_PATH)
+
+    # sample a few spectrograms + a few min_max_values
+    sampled_specs, sampled_min_max_values = select_spectrograms(specs,
+                                                                file_paths,
+                                                                min_max_values,
+                                                                5) # sample '5' spectrograms.
+
+    # gen audio from sampled spectrograms!
+    signals, _ = sound_generator.generate(sampled_specs, sampled_min_max_values)
+
+    # direct converting spectrogram samples to audio (for comparison)
+    original_signals = sound_generator.convert_spectrograms_to_audio(sampled_specs, sampled_min_max_values)
+
+    # save audio signals.
+    save_signals(signals, SAVE_DIR_GENERATED) #saved files.
+    save_signals(original_signals, SAVE_DIR_ORIGINAL)
---
+++ 4. Generating Sound Digits with a VAE/train.py
@@ -0,0 +1,65 @@
+from vae import VAE
+import os
+import numpy as np
+
+LEARNING_RATE = 0.0005
+BATCH_SIZE = 64
+#20 -> 100 epochs
+EPOCHS = 150
+
+SPECTROGRAMS_PATH = r"D:\personal projects\Music Generation\sound data\SPECTROGRAMS_SAVE_DIR"
+
+
+# load the data from the spectrograms.
+def load_fsdd(spectrograms_path):
+    x_train = [] #an empty list to be filled with spectrograms.
+    for root, _, file_names in os.walk(spectrograms_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            spectrogram = np.load(file_path) # now, had the spectrogram.
+            # shape of spectrogram -> (n_bins, n_frames) (n_bins which from stft, n_frames, 1) #1 is the extra dimension.
+            x_train.append(spectrogram) # stored in x_train list.
+    x_train = np.array(x_train) # cast to numpy array.
+    # np.newaxis adds a new axis at the end of the array, effectively creating a new axis for the channel dimension.
+    # np.newaxis -> (3000, 256, 64, "1!") #thus, treat it as a greyscale image (TRICK!)
+    x_train = x_train[..., np.newaxis] # np.newaxis is used to increase the dimensionality of an array by one unit. # -> (3000 # of data we have in dataset, 256 num bins, 64 frames, 1) # treat spectrograms as grey scale images.
+    return x_train
+
+ 
+
+
+
+#train the VAE with the FSDD dataset
+def train(x_train, learning_rate, batch_size, epochs):
+    autoencoder = VAE(
+        input_shape=(256, 64, 1),
+        conv_filters=(512, 256, 128, 64, 32),  # four conv layers. e.g first layer has 32 filters.
+        conv_kernels=(3, 3, 3, 3, 3),  # all four layers, each filter using/having size 3x3.
+        conv_strides=(2, 2, 2, 2, (2, 1)),  # step size.
+        latent_space_dim = 128
+    )
+    autoencoder.summary()
+    autoencoder.compile(learning_rate)
+    autoencoder.train(x_train, batch_size, epochs)
+    return autoencoder
+
+
+"""==================================================================================="""
+"""==================================================================================="""
+"""RUN THE MAIN()"""
+if __name__ == "__main__":
+    #MNIST dataset
+    x_train = load_fsdd(SPECTROGRAMS_PATH) #IMPLEMENT func above
+
+    # autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS) #implet func above.
+
+    #only run 500  / 10000 samples
+    autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS) #implet func above.
+
+    # save the autoencoder
+    autoencoder.save("model")
+
+    #load the model
+    # autoencoder2 = VAE.load("model")
+    #
+    # autoencoder2.summary()
